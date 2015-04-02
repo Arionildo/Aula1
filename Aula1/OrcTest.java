479fa588eacde7bc441e5ecea7a50b98f8ef461d
Arionildo <arionildo6@gmail.com>
2015-04-02 17:38:02 -0300

    Extra dia 2

---
+++ Aula1/OrcTest.java
@@ -0,0 +1,52 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A classe de teste OrcTest.
+ *
+ * @author  (seu nome)
+ * @version (um número de versão ou data)
+ */
+public class OrcTest
+{
+    /**
+     * Construtor default para a classe de teste OrcTest
+     */
+    
+    public OrcTest()
+    {
+    }
+
+    @Test
+    public void orcCom110Vida() {
+        Orc orc = new Orc();
+        int esperado = 110;
+        int resultado = orc.getVida();
+        
+        assertEquals(esperado, resultado);
+    }
+    
+    /**
+     * Define a .
+     *
+     * Chamado antes de cada método de caso de teste.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Chamado após cada método de teste de caso.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
