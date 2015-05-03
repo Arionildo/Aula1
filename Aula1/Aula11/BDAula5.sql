8f20542f4dafb5d5385c6b4c1fff69e77643656b
Arionildo <arionildo6@gmail.com>
2015-05-03 08:02:38 -0300

    Finalização BDAula5

--- Aula1/Aula11/BDAula5.sql
+++ Aula1/Aula11/BDAula5.sql
@@ -1,22 +1,25 @@
---Exercicio 4
+--Exercício 1
 select distinct
 	Situacao,
 	COUNT(1) "Valores"
 from Produto
 group by Situacao;
-
-
 --------------------------------------------------------------
+
+
+--Exercício 2
 select
 	Nome,
 	RazaoSocial
 from Cliente
+--'LTDA' PODE ESTAR TANTO NO MEIO QUANTO NO FINAL DO NOME
 where
 	RazaoSocial like '%LTDA%' OR
 	RazaoSocial like '%LTDA';
+--------------------------------------------------------------
 	
 
---------------------------------------------------------------	
+--Exercício 3	
 insert into Produto(
 	Nome,
 	PrecoCusto,
@@ -27,20 +30,23 @@ values(
 	35.67,
 	77.95,
 	'A');
+--------------------------------------------------------------
 	
 
---------------------------------------------------------------
+--Exercício 4
 select
 	p.Nome
 from
 	Produto p
+--VERIFICA SE O PRODUTO NÃO FOI PEDIDO
 where not exists(
 		select 1
 		from PedidoItem i
 		where p.IDProduto = i.IDProduto);
-		
-
 --------------------------------------------------------------
+
+
+--Exercício 5
 select top 1
 	ci.UF,
 	COUNT(1) "Maior número de clientes"
@@ -50,9 +56,11 @@ from
 where cl.IDCidade=ci.IDCidade
 group by ci.UF
 order by "Maior número de clientes" desc;
+--------------------------------------------------------------
 
 
----------------------------------------------------------------
+--Exercício 6
+--DUAS VIEW'S CRIADAS PARA FACILITAR O USO DO SELECT À SEGUIR
 create view MaiorNroClientes as
 select top 1
 	ci.UF,
@@ -77,8 +85,6 @@ order by "Menor número de clientes" asc;
 
 select * from MaiorNroClientes, MenorNroClientes;
 
-
---------------------------------------------------------------
 select
 	count(distinct ci.nome) "Total de Cidades"
 from
@@ -88,9 +94,10 @@ from
 where
 	ci.IDCidade = cl.IDCidade and
 	cl.IDCliente = p.IDCliente;
-
-
 --------------------------------------------------------------
+
+
+--Exercício 7
 select
 	p.Nome
 from
@@ -99,12 +106,17 @@ where not exists(
 		select 1
 		from ProdutoMaterial m
 		where p.IDProduto = m.IDProduto);
-
-
 --------------------------------------------------------------
+
+
+--Exercício 8
+--NO QUARTO CAMPO É CALCULADO O PrecoCusto DE ACORDO COM A
+--Quantidade PARA SE OBTER O CUSTO TOTAL DOS MATERIAIS USADOS
+--EM CADA PRODUTO
 select
 	pm.IDProduto,
 	p.Nome,
+	p.PrecoCusto,
 	sum(isnull(pm.Quantidade, 1) * m.PrecoCusto) "Custo Materiais"
 from
 	Produto p,
@@ -113,15 +125,73 @@ from
 where
 	p.IDProduto = pm.IDProduto and
 	m.IDMaterial = pm.IDMaterial
-group by pm.IDProduto, p.Nome
+group by pm.IDProduto, p.Nome, p.PrecoCusto
 order by "Custo Materiais" desc;
-
-
 --------------------------------------------------------------
---Continuar
 
 
+--Exercício 9
+--VIEW PARA FACILITAR A IMPLEMENTAÇÃO DO UPDATE À SEGUIR
+create view CustoMateriais as
+select
+	p.IDProduto,
+	sum(isnull(pm.Quantidade, 1) * m.PrecoCusto) "Custo Materiais"
+from
+	Produto p,
+	Material m,
+	ProdutoMaterial pm
+where
+	p.IDProduto = pm.IDProduto and
+	m.IDMaterial = pm.IDProdutoMaterial
+group by p.IDProduto;
+
+update Produto
+set
+	PrecoCusto = cm.[Custo Materiais]
+from
+	Produto p,
+	CustoMateriais cm
+where 
+	p.IDProduto = cm.IDProduto and
+	p.PrecoCusto < cm.[Custo Materiais];
+
+--JUSTIFICANDO O "- 0.1" À SEGUIR:
+--O CAMPO Custo Materiais POSSUI MAIS CASAS DECIMAIS DO QUE O
+--PrecoCusto QUE PODE RESULTAR EM UM VALOR ACIMA DO QUE
+--PrecoCusto POSSUI.
+select
+	Nome,
+	PrecoCusto,
+	[Custo Materiais]
+from Produto p, CustoMateriais cm
+where
+	p.IDProduto = cm.IDProduto and
+	PrecoCusto < ([Custo Materiais] - 0.1);
 --------------------------------------------------------------
+
+
+--Exercício 10
+--TANTO O NOME COMPLETO QUANTO O PRIMEIRO NOME FORAM TESTADOS,
+--PORÉM, APENAS O CAMPO Nome FOI O UTILIZADO.
+select
+	Nome
+	--SUBSTRING(Nome, 0, CHARINDEX(' ', Nome, 1))
+from Cliente
+group by Nome
+having COUNT(Nome) > 1;
+--------------------------------------------------------------
+
+
+--Exercício 11
+select
+	SUBSTRING(Nome, 0, CHARINDEX(' ', Nome, 1)),
+	COUNT(Nome) "Popularidade"
+from Cliente
+group by Nome;
+--------------------------------------------------------------
+
+
+--Exercício 12
 select
 	p.IDProduto,
 	p.Nome,
@@ -134,6 +204,7 @@ group by
 	p.IDProduto,
 	p.Nome
 order by "Vendas" desc;
+--------------------------------------------------------------
 	
 	
 -------------------------------------------------------------
