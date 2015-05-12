e6bb9beb4d8127f0a1c79499893f23c2877ba76d
Arionildo <arionildo6@gmail.com>
2015-05-12 14:47:09 -0300

    Atualização Site10

--- Aula1/Aula21/Site10/assets/js/script.js
+++ Aula1/Aula21/Site10/assets/js/script.js
@@ -1,7 +1,23 @@
 //EXERCÍCIO 01
-var a = $(".module");
-var b = $("#myList:nth-child(2)");
-var c = $("label[for='q']");
-var d = $("img[alt]").length;
-var e = $("tr:even").length;
+$(".module");
+$("#myList:nth-child(2)");
+$("#search label[for='q']");
+$("img[alt]").length;
+$("tbody tr:even").length;
+$("h2:contains('B'):contains('e')").length;
+$("#myList li:not(.current)");
+$("img:last, h3:last");
+$("#myList .current ~ li").text().length;
+//------------------------------------------------------
+
+
+//EXERCÍCIO 02
+
+//------------------------------------------------------
+
+
+//EXERCÍCIO 03
+$("div.module").last().after(
+	$("<div/>").addClass("module").append(
+		$("img:eq(0)").clone()));
 //------------------------------------------------------
